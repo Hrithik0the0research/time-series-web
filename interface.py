92a43339a51054127c660e33ba86007decc4b4e9
Hrithik0the0research <121748257+Hrithik0the0research@users.noreply.github.com>
2023-03-28 12:50:21 +0530

    Update interface.py

--- interface.py
+++ interface.py
@@ -4,20 +4,37 @@ import math
 import matplotlib.pyplot as plt
 import random 
 import skimage.io as sk
+from glob import glob
 import cv2
 import numpy as np
-st.write("Upload image")
+
+st.write("Welcome to Image reconstruction web-interface")
+demo=glob("./dataset/*")
+
+demo_file = st.selectbox("Download demo images",demo)
+print(demo_file)
+with open(demo_file, "rb") as file:
+        btn = st.download_button(
+                label="Download demo images",
+                data=file,
+                file_name="demo.png")
 name=random.randint(1,10000)
-file=st.file_uploader("choose a file")
+file=st.file_uploader("Upload an image",[".png",".jpg",".webp","jpeg"])
 if file is not None:
     file_name="fig"+str(name)+".png"
     with open(file_name, mode='bx') as f:
         f.write(file.getvalue())
     single_image=sk.imread(file_name)
     st.image(file_name)
-    lower=np.array([10,2,1,1]) #lower
-    higher=np.array([255,255,255,255]) #higher
-    mask=cv2.inRange(single_image,lower,higher)
+    if single_image.shape[2]==4:
+        lower=np.array([10,2,1,1]) #lower
+        higher=np.array([255,255,255,255]) #higher
+        mask=cv2.inRange(single_image,lower,higher)
+    else:
+        lower=np.array([10,2,1]) #lower
+        higher=np.array([255,255,255]) #higher
+        mask=cv2.inRange(single_image,lower,higher)
+
     x1,y1=mask.shape[0]//2,mask.shape[1]//2
     c1=find_contours(mask)
     c2=(x1,y1)
@@ -26,9 +43,8 @@ if file is not None:
     plt.plot(d,label="original-time-series",color="blue")
     plt.xlabel("Contour Point")
     plt.ylabel("Distance")
-    plt.legend(["Original"])
     plt.axis("off")
-    dis="distance"+str(name)+".png"
+    dis="distance"+".png"
     plt.savefig(dis)
     st.write("Radical scanning")
     st.image(dis)
@@ -36,7 +52,7 @@ if file is not None:
     first,st1=make_standard_Scaler(c1,c1.shape[0]-100)
     x,y,z=train(10,1,first,c1)
     models=[" ","LSTM","MLP"]
-    model=st.selectbox("Model",models)
+    model=st.selectbox("Choose a Model",models)
     st.write(model)
     if model!=" ":
         if model=="LSTM":
@@ -87,3 +103,10 @@ if file is not None:
     #mask=image_data
     #plt.imshow(mask)
     
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
